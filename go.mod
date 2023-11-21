a1f6219664bb080b4a937d4d6d39ce54d9f42fe0
Iggy Punx <egor.chmykhov@gmail.com>
2023-11-21 14:51:44 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/iggypunx/bookings
+
+go 1.21.4
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/iggypunx/go-course/pkg/config"
+	"github.com/iggypunx/go-course/pkg/models"
+	"github.com/iggypunx/go-course/pkg/render"
+)
+
+// repository used by the handlers
+var Repo *Repository
+
+// repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the about home handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+
+	stringMap["remote_ip"] = remoteIP
+
+	// send the data to the template
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
